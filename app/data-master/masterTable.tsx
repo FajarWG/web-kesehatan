817ec904aac092322d7415feb4680f6b0663a29e
FajarWG <fajarwahyugumelar@gmail.com>
2024-02-12 20:33:55 +0700

    Update  table

--- app/data-master/masterTable.tsx
+++ app/data-master/masterTable.tsx
@@ -3,27 +3,33 @@
 import React from 'react';
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
 import {
-    Table,
-    TableHead,
-    TableRow,
-    TableHeaderCell,
-    TableBody,
-    TableCell,
-    Text
-  } from '@tremor/react';
-  
-  interface DataMaster {
-    id: number;
-    nama_obat: string;
-    pemakaian: string;
-    penerimaan: string;
-    sisa_stok: number;
-    bulan: number;
-    tahun: number;
-  }
-  
-  export default function MasterTable({ data, setModal }: { data: DataMaster[], setModal: any }) {
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Text
+} from '@tremor/react';
 
+interface DataMaster {
+  id: number;
+  nama_obat: string;
+  stok_awal: number;
+  pemakaian: string;
+  penerimaan: string;
+  sisa_stok: number;
+  bulan: number;
+  tahun: number;
+}
+
+export default function MasterTable({
+  data,
+  setModal
+}: {
+  data: DataMaster[];
+  setModal: any;
+}) {
   const [isLoading, setIsLoading] = React.useState(false);
 
   async function deleteData(id: string) {
@@ -37,51 +43,49 @@ import {
     });
   }
 
-    return (
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>No</TableHeaderCell>
-            <TableHeaderCell>Nama Obat</TableHeaderCell>
-            <TableHeaderCell>Pemakaian</TableHeaderCell>
-            <TableHeaderCell>Penerimaan</TableHeaderCell>
-            <TableHeaderCell>Sisa Stok</TableHeaderCell>
-            <TableHeaderCell>Aksi</TableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((user, index) => (
-            <TableRow key={user.id}>
-              <TableCell>{index + 1}</TableCell>
-              <TableCell>{user.nama_obat}</TableCell>
-              <TableCell>
-                <Text>{user.pemakaian}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{user.penerimaan}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{user.sisa_stok}</Text>
-              </TableCell>
-              <TableCell className='flex gap-2'>
-
-                <a href={`/data-master/editData?id=${user.id}`}>
-                  <PencilIcon className="w-5 h-5 text-gray-500" />
-                </a>
-                <button onClick={() => {
-                  deleteData(user.id.toString())
-                  setModal(true)
-                }
-                } disabled={isLoading}>
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeaderCell>No</TableHeaderCell>
+          <TableHeaderCell>Nama Obat</TableHeaderCell>
+          <TableHeaderCell>Stok Awal</TableHeaderCell>
+          <TableHeaderCell>Pemakaian</TableHeaderCell>
+          <TableHeaderCell>Penerimaan</TableHeaderCell>
+          <TableHeaderCell>Aksi</TableHeaderCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((user, index) => (
+          <TableRow key={user.id}>
+            <TableCell>{index + 1}</TableCell>
+            <TableCell>{user.nama_obat}</TableCell>
+            <TableCell>
+              <Text>{user.stok_awal}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>{user.pemakaian}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>{user.penerimaan}</Text>
+            </TableCell>
+            <TableCell className="flex gap-2">
+              <a href={`/data-master/editData?id=${user.id}`}>
+                <PencilIcon className="w-5 h-5 text-gray-500" />
+              </a>
+              <button
+                onClick={() => {
+                  deleteData(user.id.toString());
+                  setModal(true);
+                }}
+                disabled={isLoading}
+              >
                 <TrashIcon className="w-5 h-5 text-gray-500" />
-
-                </button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        
-      </Table>
-    );
-  }
-  
+              </button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
