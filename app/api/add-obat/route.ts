5cea43b7ebd6d2d65387b69e206a2ed55e5ea666
FajarWG <fajarwahyugumelar@gmail.com>
2024-02-13 21:51:40 +0700

    Fix table add and edit data

--- app/api/add-obat/route.ts
+++ app/api/add-obat/route.ts
@@ -1,12 +1,12 @@
-import { prisma } from "../../../lib/prisma";
-import { NextResponse } from "next/server";
+import { prisma } from '../../../lib/prisma';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   const body = await req.json();
 
   const obat = await prisma.laporanTransaksi.createMany({
     data: body.data,
-    skipDuplicates: true,
+    skipDuplicates: true
   });
 
   return NextResponse.json(obat);
--- app/data-master/addTable.tsx
+++ app/data-master/addTable.tsx
@@ -1,143 +1,177 @@
 'use client';
 
 import React, { useEffect } from 'react';
-import Select from "react-select";
-import { PencilIcon, PlusCircleIcon, TrashIcon } from '@heroicons/react/24/outline';
+import Select from 'react-select';
 import {
-    Table,
-    TableHead,
-    TableRow,
-    TableHeaderCell,
-    TableBody,
-    TableCell,
-    Button,
-    NumberInput
-  } from '@tremor/react';
+  PencilIcon,
+  PlusCircleIcon,
+  TrashIcon
+} from '@heroicons/react/24/outline';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Button,
+  NumberInput
+} from '@tremor/react';
 
-  
-  export default function AddTable({ data, namaObat, setModal, searchDate, setTambahData }: { data: any, namaObat: any, setModal: any, searchDate: any, setTambahData: any }) {
+export default function AddTable({
+  data,
+  namaObat,
+  setModal,
+  searchDate,
+  setTambahData
+}: {
+  data: any;
+  namaObat: any;
+  setModal: any;
+  searchDate: any;
+  setTambahData: any;
+}) {
+  const [dataObat, setDataObat] = React.useState<any>(data);
+  const [namaObats, setNamaObats] = React.useState<any>([
+    {
+      nama_obat: 'ADS 0,3 ml'
+    },
+    {
+      nama_obat: 'ADS 3ml'
+    },
+    {
+      nama_obat: 'Alat Suntik Sekali Pakai 10ml'
+    }
+  ]);
 
-    const [dataObat, setDataObat] = React.useState<any>(data);
-    const [namaObats, setNamaObats] = React.useState<any>([
+  const [namaObatDump, setNamaObatDump] = React.useState<any>(namaObat);
+
+  const onAddData = () => {
+    setDataObat([
+      ...dataObat,
       {
-        nama_obat: 'ADS 0,3 ml',
-      },
-      {
-        nama_obat: 'ADS 3ml',
-      },{
-        nama_obat: 'Alat Suntik Sekali Pakai 10ml'
+        nama_obat: '',
+        pemakaian: 0,
+        penerimaan: 0,
+        sisa_stok: 0,
+        stok_awal: 0,
+        jumlah_stok: 0,
+        bulan: searchDate === '' ? 0 : parseInt(searchDate.split('-')[1]),
+        tahun: searchDate === '' ? 0 : parseInt(searchDate.split('-')[0])
       }
     ]);
+  };
 
-    const [namaObatDump, setNamaObatDump] = React.useState<any>(namaObat);
-
-
-    const onAddData = () => {
-      setDataObat([...dataObat, {
-          nama_obat: '',
-          pemakaian: 0,
-          penerimaan: 0,
-          sisa_stok: 0,
-          stok_awal: 0,
-          jumlah_stok: 0,
-          bulan: searchDate === '' ? 0 : parseInt(searchDate.split('-')[1]),
-          tahun: searchDate === '' ? 0 : parseInt(searchDate.split('-')[0]),
-      }])
-    }
-
-    const handleInputChange = (index: any, field: any, value: any) => {
-      const newData = [...dataObat];
+  const handleInputChange = (index: any, field: any, value: any) => {
+    const newData = [...dataObat];
+    if (field === 'nama_obat') {
+      newData[index][field] = value.nama_obat;
+    } else {
       newData[index][field] = value;
-      setDataObat(newData);
-    };
-
-    const filterNamaObat = (value: any) => {
-      let filtered = namaObatDump.filter((item: any) => item.nama_obat.toLowerCase().includes(value.toLowerCase()))
-      filtered = filtered.slice(0, 10)
-      setNamaObats(filtered)
     }
+    setDataObat(newData);
+  };
 
-    useEffect(() => {
-        const [year, month] = searchDate.split('-');
-        if(year === undefined || month === undefined) return;
-        console.log(year, month)
-        setDataObat(dataObat.map((item: any) => {
-            return {
-                ...item,
-                bulan: parseInt(month),
-                tahun: parseInt(year)
-            }
-        }))
+  const filterNamaObat = (value: any) => {
+    let filtered = namaObatDump.filter((item: any) =>
+      item.nama_obat.toLowerCase().includes(value.toLowerCase())
+    );
+    filtered = filtered.slice(0, 10);
+    setNamaObats(filtered);
+  };
+
+  useEffect(() => {
+    const [year, month] = searchDate.split('-');
+    if (year === undefined || month === undefined) return;
+    console.log(year, month);
+    setDataObat(
+      dataObat.map((item: any) => {
+        return {
+          ...item,
+          bulan: parseInt(month),
+          tahun: parseInt(year)
+        };
+      })
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [searchDate])
+  }, [searchDate]);
 
-    useEffect(() => {
-      setNamaObatDump(namaObat)
-      setNamaObats(namaObat.slice(0, 10))
-    },[namaObat])
+  useEffect(() => {
+    setNamaObatDump(namaObat);
+    setNamaObats(namaObat.slice(0, 10));
+  }, [namaObat]);
 
-
-    return (
-      <>
+  return (
+    <>
       <Table>
         <TableHead>
           <TableRow>
             <TableHeaderCell>No</TableHeaderCell>
             <TableHeaderCell>Nama Obat</TableHeaderCell>
+            <TableHeaderCell>Stok Awal</TableHeaderCell>
             <TableHeaderCell>Pemakaian</TableHeaderCell>
             <TableHeaderCell>Penerimaan</TableHeaderCell>
-            <TableHeaderCell>Sisa Stok</TableHeaderCell>
           </TableRow>
         </TableHead>
-        <TableBody >
+        <TableBody>
           {dataObat.map((user: any, index: any) => (
             <TableRow key={index}>
               <TableCell>{index + 1}</TableCell>
-              <TableCell className='relative'>
+              <TableCell className="relative">
                 <Select
                   options={namaObats}
-                  onChange={(value) => handleInputChange(index, 'nama_obat', value)}
+                  onChange={(value) =>
+                    handleInputChange(index, 'nama_obat', value)
+                  }
                   getOptionLabel={(option: any) => option.nama_obat}
                   getOptionValue={(option: any) => option.nama_obat}
-                  placeholder='Masukkan Nama Obat'
+                  placeholder="Masukkan Nama Obat"
                   onInputChange={(value) => filterNamaObat(value)}
                 />
-                </TableCell>
+              </TableCell>
               <TableCell>
-                <NumberInput className='' 
-                  placeholder='Masukkan Jumlah Pemakaian' 
-                  onValueChange={(value) => handleInputChange(index, 'pemakaian', value)}
+                <NumberInput
+                  className=""
+                  placeholder="Masukkan Jumlah Stok Awal"
+                  onValueChange={(value) =>
+                    handleInputChange(index, 'stok_awal', value)
+                  }
                 />
               </TableCell>
               <TableCell>
-                <NumberInput className='' 
-                  placeholder='Masukkan Jumlah Penerimaan'
-                  onValueChange={(value) => handleInputChange(index, 'penerimaan', value)}
+                <NumberInput
+                  className=""
+                  placeholder="Masukkan Jumlah Pemakaian"
+                  onValueChange={(value) =>
+                    handleInputChange(index, 'pemakaian', value)
+                  }
                 />
               </TableCell>
               <TableCell>
-                <NumberInput className='' 
-                placeholder='Masukkan Jumlah Sisa Stok'
-                onValueChange={(value) => handleInputChange(index, 'sisa_stok', value)}
+                <NumberInput
+                  className=""
+                  placeholder="Masukkan Jumlah Penerimaan"
+                  onValueChange={(value) =>
+                    handleInputChange(index, 'penerimaan', value)
+                  }
                 />
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <div className='flex flex-row justify-end gap-5 mt-5'>
+      <div className="flex flex-row justify-end gap-5 mt-5">
         <Button onClick={onAddData}>
-          <PlusCircleIcon className='w-5 h-5' />
+          <PlusCircleIcon className="w-5 h-5" />
         </Button>
-        <Button onClick={() => {
-          setTambahData(dataObat)
-        }
-        }>
+        <Button
+          onClick={() => {
+            setTambahData(dataObat);
+          }}
+        >
           Simpan Data
         </Button>
       </div>
-
-      </>
-    );
-  }
-  
+    </>
+  );
+}
