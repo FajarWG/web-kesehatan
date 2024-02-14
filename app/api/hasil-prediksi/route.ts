dfee184728c36d76fa4a8f20bffaf3961997aa14
FajarWG <fajarwahyugumelar@gmail.com>
2024-02-15 00:58:08 +0700

    asc hasil prediksi

--- app/api/hasil-prediksi/route.ts
+++ app/api/hasil-prediksi/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(req: Request) {
   const hasilPrediksi = await prisma.pengadaan.findMany({
     orderBy: {
-      id: 'desc'
+      createdAt: 'asc'
     },
     take: 1
   });
